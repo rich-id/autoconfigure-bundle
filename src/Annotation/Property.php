47b1d6644d677e3ef9323929e92e3be071079471
Nicolas Guilloux <nicolas.guilloux@protonmail.com>
2021-12-23 10:04:41 +0100

    Remove useless uses

--- src/Annotation/Property.php
+++ src/Annotation/Property.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace RichId\AutoconfigureBundle\Annotation;
 
 use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
-use Doctrine\ORM\Mapping\Annotation;
 
 /**
  * Class Property.
